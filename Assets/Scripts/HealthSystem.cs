49b393753d733fd71b4ddbc96a0d895adef26b1d
Mehmet Berkay Coruk <89832806+MehmetBerkayC@users.noreply.github.com>
2023-08-05 20:17:59 +0300

    Working on a health system

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem 
+{
+    int maxHealth;
+    int currentHealth;
+    bool isAlive;
+
+    // Constructor
+    public HealthSystem(int maxHealth)
+    {
+        this.maxHealth = maxHealth;
+        currentHealth = maxHealth;
+        isAlive = true;
+    }
+
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public void Heal(int amount)
+    {
+        if (isAlive && currentHealth < maxHealth)
+        {
+            currentHealth += amount;
+        }
+        else
+        {
+            Debug.Log("Entity cannot be healed!");
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (isAlive)
+        {
+            currentHealth -= amount;
+        }
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            isAlive = false; 
+        }
+    }
+}
